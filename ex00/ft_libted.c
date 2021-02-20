3fae6347701e60a9a3e1a8db9a309da042e702c6
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-20 18:50:47 -0500

    merge

--- ex00/ft_libted.c
+++ ex00/ft_libted.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 14:52:26 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/20 18:46:55 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/20 18:48:44 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,63 @@ int	ft_sqrt(int nb)
 			return (1);
 		}
 	return 0;
+}
+
+void	ft_putmax()
+{	
+	int row;
+	int col;
+	int index;
+
+	
+	row = 0;
+	col = 0;	
+	while(++col <= g_RC)
+	{
+		printf("FIRST LOOP row: %d, col: %d\n", row, col);
+		if(tab[row][col] == g_RC + '0')
+		{
+			index = 0;
+			while(++index <= g_RC)
+			{
+				tab[index][col] = index + '0';
+			}
+		}
+	}
+	while(++row <= g_RC)
+	{
+		printf("SECOND LOOP row: %d, col: %d\n", row, col);
+		if(tab[row][col] == g_RC + '0')
+		{
+			index = 0;
+			while(++index <= g_RC)
+			{
+				tab[row][index] = g_RC - index + 1 + '0';
+			}
+		}
+	}	
+	while(--col >= 1)
+	{
+		printf("THIRD LOOP row: %d, col: %d\n", row, col);
+		if(tab[row][col] == g_RC + '0')
+		{
+			index = 0;
+			while(++index <= g_RC)
+			{
+				tab[index][col] = g_RC - index + 1 + '0';
+			}
+		}
+	}
+	while(--row >= 1)
+	{
+		printf("FOUR LOOP row: %d, col: %d\n", row, col);
+		if(tab[row][col] == g_RC + '0')
+		{
+			index = 0;
+			while(++index <= g_RC)
+			{
+				tab[row][index] = index + '0';
+			}
+		}
+	}	
 }
