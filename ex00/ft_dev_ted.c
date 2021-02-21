94ba8f31b706b4a8ec8f0aa1692b757ce7cfba56
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-21 16:09:25 -0500

    last algo

--- ex00/ft_dev_ted.c
+++ ex00/ft_dev_ted.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 07:40:15 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/21 14:58:23 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/21 15:43:53 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,39 +19,10 @@ void	ft_check_duplicate(char **tab, char ***tab_char)
 
 	row = 0;
 	while (++row <= g_RC)
-	{	
+	{
 		col = 0;
 		while (++col <= g_RC)
-		{
-			// printf("BEFORE: Tab[%d][%d] -- tab_char[%d][%d] : %s\n",row, col, row -1 , col - 1, tab_char[row -1 ][col - 1]);
-			ft_compare(tab, tab_char[row - 1 ][col - 1], col, row);
-			printf("BEFORE: tab_char[%d][%d] : %s\n", row -1 , col - 1, tab_char[row -1 ][col - 1]);
-
-		}
-	}
-
-	row = 0;
-	while (++row <= g_RC)
-	{	
-		col = 0;
-		while (++col <= g_RC)
-		{
-			// printf("BEFORE: Tab[%d][%d] -- tab_char[%d][%d] : %s\n",row, col, row -1 , col - 1, tab_char[row -1 ][col - 1]);
-			set_possible_char(tab, tab_char[row - 1 ][col - 1], col, row);
-			printf("MID: tab_char[%d][%d] : %s\n", row -1 , col - 1, tab_char[row -1 ][col - 1]);
-		}
-	}
-	row = 0;
-	while (++row <= g_RC)
-	{	
-		col = 0;
-		while (++col <= g_RC)
-		{
-			// printf("BEFORE: Tab[%d][%d] -- tab_char[%d][%d] : %s\n",row, col, row -1 , col - 1, tab_char[row -1 ][col - 1]);
-			ft_compare(tab, tab_char[row - 1 ][col - 1], col, row);
-			printf("BEFORE: tab_char[%d][%d] : %s\n", row -1 , col - 1, tab_char[row -1 ][col - 1]);
-
-		}
+			ft_compare(tab, tab_char[row - 1][col - 1], col, row);
 	}
 }
 
@@ -65,7 +36,147 @@ void	ft_set_full_copy(char **tab, char **tab_origin)
 	{
 		col = -1;
 		while (++col != g_RC + 2)
-				tab[row][col] = tab_origin[row][col];
+			tab[row][col] = tab_origin[row][col];
 	}
 }
 
+int		ft_resolve_test(char **tab,char ***tab_str, int row, int col)
+{
+	char	*str;
+	char	**tab_test;
+	int		i;
+	int		ret;
+	
+	i = 0;
+	ret = 0;
+	tab_test = ft_malloc_tab_memory();
+	ft_set_full_copy(tab_test, tab);
+	if (tab[col][row] == '&')
+		str = tab_str[col][row];
+	while (str[i] != '\0')
+	{
+		if (str[i] != '.')
+		{
+			tab_test[col][row] == str[i];
+			if (ft_compare_last(tab_test, str, col, row) != 0)
+				i++;
+			else
+				break ;
+		}
+		i++;
+	}
+	ft_free_tab_test_memory(tab_test);
+}
+
+int     ft_last_resolve(char **tab, char ***tab_str)
+{
+	int i;
+	int row;
+	int col;
+
+	row  = -1;
+	while (++row <= g_RC)
+	{
+		col = -1;
+		while (++col <= g_RC)
+		ft_resolve_test(tab, tab_str, row, col);
+	}
+}
+
+// int     ft_last_resolve(char **tab, char ***tab_str)
+// {
+// 	char **tab_test;
+// 	int i;
+// 	int cpt;
+// 	char *str;
+// 	int row;
+// 	int col;
+// 	int val_return;
+// 	val_return = 0;
+// 	row  = -1;
+// 	col = -1;
+// 	i = 0;
+// 	cpt = 0;
+
+// 	tab_test = ft_malloc_tab_memory();
+// 	ft_set_full_copy(tab_test, tab);
+// 	ft_free_tab_test_memory(tab_test);
+
+// 	while (++row <= g_RC)
+// 	{
+// 		while (++col <= g_RC)
+// 		{
+// 			if (tab[col][row] == '&')
+// 			{
+// 				str = tab_str[col][row];
+// 			}
+// 			while (str[i] != '\0')
+// 			{
+// 				if (str[i] != '.')
+// 				{
+// 					tab_test[col][row] == str[i];
+// 					val_return = ft_compare_last(tab_test, str, col, row);
+// 					if (val_return != 0)
+// 					{
+// 						i++;
+// 					}
+// 					else
+// 					{
+// 						break ;
+// 					}
+// 				}
+// 				i++;
+// 			}
+// 		}
+// 	}
+// 	ft_free_tab_test_memory(tab_test);
+// }
+
+int     ft_last_resolve(char **tab, char ***tab_str)
+{
+	char **tab_test;
+	int i;
+	int cpt;
+	char *str;
+	int row;
+	int col;
+	int val_return;
+	val_return = 0;
+	row  = -1;
+	col = -1;
+	i = 0;
+	cpt = 0;
+
+	tab_test = ft_malloc_tab_memory();
+	ft_set_full_copy(tab_test, tab);
+	ft_free_tab_test_memory(tab_test);
+
+	while (++row <= g_RC)
+	{
+		while (++col <= g_RC)
+		{
+			if (tab[col][row] == '&')
+			{
+				str = tab_str[col][row];
+			}
+			while (str[i] != '\0')
+			{
+				if (str[i] != '.')
+				{
+					tab_test[col][row] == str[i];
+					val_return = ft_compare_last(tab_test, str, col, row);
+					if (val_return != 0)
+					{
+						i++;
+					}
+					else
+					{
+						break ;
+					}
+				}
+				i++;
+			}
+		}
+	}
+	ft_free_tab_test_memory(tab_test);
+}
